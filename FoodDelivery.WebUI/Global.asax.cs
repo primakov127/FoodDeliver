8bfe93d6aa93b7fcb0d320b0ff08d807955647ce
Max Primakov <primakov127@gmail.com>
2020-03-23 21:22:19 +0300

    Added removing from cart ability and binding of cart model.

--- FoodDelivery.WebUI/Global.asax.cs
+++ FoodDelivery.WebUI/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FoodDelivery.Domain.Entities;
+using FoodDelivery.WebUI.Infrastructure.Binders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +15,7 @@ namespace FoodDelivery.WebUI
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
 }
