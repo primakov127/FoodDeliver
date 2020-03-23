51a2359c307886eb18756562de770026bae3b5f9
Max Primakov <primakov127@gmail.com>
2020-03-23 19:13:07 +0300

    Added cart orders

---
+++ FoodDelivery.Domain/Entities/Cart.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(p => p.Product.ProductId == product.ProductId)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product product)
+        {
+            lineCollection.RemoveAll(p => p.Product.ProductId == product.ProductId);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(p => p.Product.Price * p.Quantity);
+        }
+
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ FoodDelivery.WebUI/Controllers/CartController.cs
@@ -0,0 +1,65 @@
+﻿using FoodDelivery.Domain.Abstract;
+using FoodDelivery.Domain.Entities;
+using FoodDelivery.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodDelivery.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
