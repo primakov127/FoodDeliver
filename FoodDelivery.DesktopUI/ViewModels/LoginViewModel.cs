ae201b53cb1420a7c7dfa6607a5c56f80d98ab2d
Max Primakov <primakov127@gmail.com>
2020-05-05 11:48:15 +0300

    Added LoginView and LoginViewModel.

---
+++ FoodDelivery.DesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery.DesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string userName;
+        private string password;
+
+        public string UserName
+        {
+            get { return userName; }
+            set
+            {
+                userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+
+        public bool CanLogIn
+        {
+            get
+            {
+                bool output = false;
+
+                if (userName?.Length > 0 && password?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
+
+        public void LogIn()
+        {
+            
+        }
+    }
+}
--- FoodDelivery.DesktopUI/ViewModels/ShellViewModel.cs
+++ FoodDelivery.DesktopUI/ViewModels/ShellViewModel.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace FoodDelivery.DesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel loginVM;
 
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            this.loginVM = loginVM;
+            ActivateItem(this.loginVM);
+        }
     }
 }
