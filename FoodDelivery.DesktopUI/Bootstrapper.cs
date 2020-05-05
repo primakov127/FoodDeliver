150afa7557c8b57ec50ca19d8348aa20e9189203
Max Primakov <primakov127@gmail.com>
2020-05-05 10:33:39 +0300

    Added new WPF project of Desktop application. Configurated it with Caliburn.Micro and included Simple Container DI system.

---
+++ FoodDelivery.DesktopUI/Bootstrapper.cs
@@ -0,0 +1,57 @@
+﻿using Caliburn.Micro;
+using FoodDelivery.DesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FoodDelivery.DesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            container.Instance(container);
+
+            container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+    }
+}
