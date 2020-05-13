11c9c999fcd51ee3fb65538fca46e7087f81fbd7
Max Primakov <primakov127@gmail.com>
2020-05-13 19:22:30 +0300

    Some chages in MenuView.

--- FoodDelivery.DesktopUI/ViewModels/CookViewModel.cs
+++ FoodDelivery.DesktopUI/ViewModels/CookViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Caliburn.Micro;
+using FoodDelivery.DesktopUI.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,17 @@ namespace FoodDelivery.DesktopUI.ViewModels
 {
     public class CookViewModel : Screen
     {
+        private BindingList<OrderDisplayModel> orders;
+
+        public CookViewModel()
+        {
+
+        }
+
+        public BindingList<OrderDisplayModel> Orders
+        {
+            get => orders;
+            set => Set(ref orders, value);
+        }
     }
 }
